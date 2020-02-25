89bf248dbcbb4e2391d8d8e34db9ac541abbde7b
Bhushan Suwal <bhushan_suwal@live.com>
2020-02-25 15:23:42 -0500

    more package setup

--- src/GerryChain.jl
+++ src/GerryChain.jl
@@ -1,5 +1,13 @@
 module GerryChain
 
-greet() = print("Hello World!")
+export
+
+AbstractGraph, Graph, Partition,
+
+get_attributes, get_populations_and_assignments, get_district_nodes, get_district_populations,
+get_district_adj_and_cut_edges
+
+include("./graph.jl")
+include("./partition.jl")
 
 end # module

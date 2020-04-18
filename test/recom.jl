d5492980ec99e5ac67f38435d5337d32a578fe3c
Bhushan Suwal <bhushan_suwal@live.com>
2020-04-18 00:47:07 -0400

    package doesn't export private recom functions

--- test/recom.jl
+++ test/recom.jl
@@ -1,3 +1,4 @@
+using DataStructures
 
 @testset "Recom tests" begin
     graph = BaseGraph(filepath, "population", "assignment")
@@ -7,13 +8,13 @@
         edges = [graph.adj_matrix[1,5], graph.adj_matrix[5,6], graph.adj_matrix[2,6],
                  graph.adj_matrix[2,3], graph.adj_matrix[3,7], graph.adj_matrix[3,4],
                  graph.adj_matrix[4,8]]
-        mst = build_mst(graph, BitSet(nodes), BitSet(edges))
+        mst = GerryChain.build_mst(graph, BitSet(nodes), BitSet(edges))
         stack = Stack{Int}()
         component_container = BitSet([])
         cut_edge = graph.adj_matrix[2,3]
-        component = traverse_mst(mst, 2, 3, stack, component_container)
+        component = GerryChain.traverse_mst(mst, 2, 3, stack, component_container)
         @test component == BitSet([1, 2, 5, 6])
-        component = traverse_mst(mst, 1, 5, stack, component_container)
+        component = GerryChain.traverse_mst(mst, 1, 5, stack, component_container)
         @test component == BitSet([1])
     end
 end

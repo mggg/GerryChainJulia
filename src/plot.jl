306175a68176db077e05884c0317a1e8b34f612d
Matthew Sun <matthewkol186@gmail.com>
2020-07-15 16:09:53 -0400

    Write functions that call PyPlot to make boxplots of District/PlanScores

--- src/plot.jl
+++ src/plot.jl
@@ -1,34 +1,29 @@
-
-function score_boxplot(chain_data::ChainScoreData,
-                       score_key::Union{DistrictAggregte, DistrictScore},
-                       sort_by_score=false)
+function score_boxplot(score_values::Array{S, 2}, sort_by_score=true) where {S<:Number}
+    """ DistrictScore. Expects data with dimensions (# of steps, districts)
     """
-    """
-    
-    # graph
-end
-
-
-function score_boxplot(chain_data::ChainScoreData,
-                       score_key::Union{PlanScore})
-    """
-    """
-    index = findfirst(s -> s.name == score_name, chain_data.scores)
-    if index == 0
-        throw(ArgumentError("No score with requested name found."))
+    if sort_by_score
+        # sort columns of by the mean
+        score_values = sortslices(score_values, dims=1, lt=(x,y) -> isless(mean(x), mean(y)))
     end
-    score_boxplot(chain_data.step_values, chain_data.scores[index])
+    plt.boxplot(score_values)
+end
+
+function score_boxplot(score_values::Array{S, 1}) where {S<:Number}
+    """ PlanScore
+    """
+    # graph
+    plt.boxplot(score_values)
 end
 
 
-function score_boxplot(chain_data::ChainScoreData, score_key::String)
+function score_boxplot(chain_data::ChainScoreData, score_name::String)
     """
     """
     index = findfirst(s -> s.name == score_name, chain_data.scores)
-    if index == 0
-        throw(ArgumentError("No score with requested name found."))
-    elseif chain_data.scores[index] isa CompositeScore
+    if chain_data.scores[index] isa CompositeScore
         throw(ArgumentError("Cannot make a boxplot of a CompositeScore."))
     end
-    score_boxplot(chain_data.step_values, chain_data.scores[index])
+    score_boxplot(get_score_values(chain_data, score_name))
+    plt.xlabel("Indexed districts")
+    plt.ylabel(score_name)
 end

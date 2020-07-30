73bc5bf4ffd8d451ec07babc4be208a9cb2c18e2
Matthew Sun <matthewkol186@gmail.com>
2020-07-29 17:24:01 -0700

    Create simple graph from polygons (#35)
    * Create simple graph and tests
    
    * Clean up comment on adjacency_exists function
    
    * Pre-empt the possibility for self-loops and don't create redundant edges
    
    * Rename some functions for clarity

--- src/graph.jl
+++ src/graph.jl
@@ -81,6 +81,72 @@ function rook_intersection(intersection)::Bool
 end
 
 
+function adjacent(node₁_polygons::Array{LibGEOS.Polygon, 1},
+                  node₂_polygons::Array{LibGEOS.Polygon, 1},
+                  adjacency_fn::Function)::Bool
+    """ Returns true if there exists an adjacency between any of the polygons
+        in node 1 and any of the polygons in node 2. The user provides
+        an adjacency_fn (such as queen_intersection or rook_intersection) to
+        evaluate the output of the LibGEOS.intersection() function.
+    """
+    for p₁ in node₁_polygons
+        for p₂ in node₂_polygons
+            intersection = LibGEOS.intersection(p₁, p₂)
+            if adjacency_fn(intersection)
+                return true
+            end
+        end
+    end
+    return false
+end
+
+
+function simple_graph_from_polygons(polygons::Array{Array{LibGEOS.Polygon, 1}, 1},
+                                    minimum_bounding_rects::Array{Tuple{Array{Float64,1},Array{Float64,1}},1},
+                                    adjacency::String="rook")::SimpleGraph
+    """ Constructs a simple graph from polygons and the associated minimum
+        bounding rectangles.
+
+        Arguments:
+
+        polygons                : Each entry in the array represents one node,
+                                  which is itself represented as an Array of
+                                  LibGEOS.Polygon objects
+        minimum_bounding_rects  : An Array of Tuples, where each tuple of
+                                  the array has the coordinates of the lower left
+                                  corner of the minimum bounding rectangle and
+                                  the coordinates of the upper left corner. The
+                                  indices of this array should match up with
+                                  the indices of the polys array.
+        adjacency               : A String that is either "queen" or "rook"
+                                  representing the style of adjacency the user
+                                  prefers.
+    """
+    if !(adjacency in ["queen", "rook"])
+        throw(ArgumentError("Adjacency must be either \"queen\" or \"rook\"."))
+    elseif length(polygons) != length(minimum_bounding_rects)
+        throw(ArgumentError("Length of polygons array must match length of minimum bounding rectangles array."))
+    end
+    adjacency_fn = adjacency == "queen" ? queen_intersection : rook_intersection
+    graph = SimpleGraph(length(polygons))
+    rtree = create_rtree(minimum_bounding_rects)
+
+    for (i, p) in enumerate(polygons)
+        # get all candidate nodes
+        lower_left = minimum_bounding_rects[i][1]
+        upper_right = minimum_bounding_rects[i][2]
+        candidate_idxs = LibSpatialIndex.intersects(rtree, lower_left, upper_right)
+        for c_idx in candidate_idxs
+            # no self loops
+            if c_idx != i && !has_edge(graph, i, c_idx) && adjacent(p, polygons[c_idx], adjacency_fn)
+                add_edge!(graph, i, c_idx)
+            end
+        end
+    end
+    return graph
+end
+
+
 function read_table(filepath::AbstractString)::Shapefile.Table
     """ Read table from shapefile. If a .shp and a .dbf file of the same name
         are not found, then we throw an error.
